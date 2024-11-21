f150f191c5a83a7fc9325e3a38c74f2508243dfd
FaronicsProServices <proservices@faronics.com>
2024-11-21 19:39:36 +0530

    Create ListAllBluetoothDevices.ps1

---
+++ Bluetooth/ListAllBluetoothDevices.ps1
@@ -0,0 +1,6 @@
+# Get the list of connected Bluetooth devices 
+$bluetoothDevices = Get-WmiObject -Query "SELECT * FROM Win32_PnPEntity WHERE PNPClass = 'Bluetooth'" | Select-Object Name 
+# Display the list of Bluetooth devices 
+foreach ($device in $bluetoothDevices) { 
+    Write-Host "Bluetooth Device: $($device.Name)" 
+}
